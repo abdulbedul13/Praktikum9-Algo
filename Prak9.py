3381da33ce04c0517bec469c7f69ff756213670b
Abdullah Hudzafah <96789420+abdulbedul13@users.noreply.github.com>
2021-12-30 22:15:46 +0700

    Update Prak9.py

--- Prak9.py
+++ Prak9.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Dec 28 20:43:31 2021
+
 @author: Abdullah
 """
 
@@ -12,3 +13,14 @@ def reverse(list):
 
 list_abdul = [5, 10, 7, 4, 15, 3]
 print("List:", list_abdul, "\n" + "Hasil reverse list menjadi tuple:", reverse(list_abdul))
+
+# ELEMEN KOMPETENSI 3
+def elkom3(list_abdul):
+    hasil = 1
+    for x in list_abdul:
+        hasil = hasil * x
+    print("List:", list_abdul)
+    print("Hasil kali value list:", hasil)
+
+
+elkom3([5, 3, -8])
